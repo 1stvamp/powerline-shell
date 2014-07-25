4b54406fa60567228802d5327be0543f98f54705
Wes Mason <wes@1stvamp.org>
2014-07-25 15:29:05 -0300

    Add exit code to root segment

--- segments/root.py
+++ segments/root.py
@@ -6,9 +6,12 @@ def add_root_indicator_segment():
     }
     bg = Color.CMD_PASSED_BG
     fg = Color.CMD_PASSED_FG
+    indicator = root_indicators[powerline.args.shell]
     if powerline.args.prev_error != 0:
         fg = Color.CMD_FAILED_FG
         bg = Color.CMD_FAILED_BG
-    powerline.append(root_indicators[powerline.args.shell], fg, bg)
+        indicator = " %d " % (powerline.args.prev_error,)
+
+    powerline.append(indicator, fg, bg)
 
 add_root_indicator_segment()
